a111a42295c1ea26bfd02e7e1282377c92b73615
Suttipot Prathoumthong - 6609650699 <144650601+SuttipotPrathoumthong@users.noreply.github.com>
2026-03-18 17:44:27 +0700

    f Reuse StudentList

--- Lab07_Group05/App.js
+++ Lab07_Group05/App.js
@@ -3,14 +3,27 @@ import StudentList from './components/StudentList';
 
 function App() {
 
-  const studentList = [{name: "Thammasat Rakdham", nickname: "Deng"},
+  const studentList = [
+    {name: "Thammasat Rakdham", nickname: "Deng"},
     {name: "Sarah Smith", nickname: "Dhum"},
     {name: "Mayuri Booksak", nickname: "Yuri"}
+  ];
+
+  const groupMembers = [
+    {name: "นายสุรบดี ผาสุข", nickname: "ไม้"},
+    {name: "นายจุติณัฏฐ์ รัตนะมงคลกุล", nickname: "บอมบ์"},
+    {name: "นายภูวฤทธิ์ เจริญพล", nickname: "เติร์ด"},
+    {name: "นายรพินทร์ นะราช", nickname: "เอม"},
+    {name: "นายศุภณัฐ ตั้งกจิวุฒิกุล", nickname: "โปโล"},
+    {name: "นายสุทธิพจน์ ประทุมทอง", nickname: "เอม"}
   ];
 
   return (
     <div className="App">
       <StudentList studentList={studentList} />
+      <hr style={{ margin: '20px 0' }} />
+      {/* Reuse Component */}
+      <StudentList studentList={groupMembers} />
     </div>
   );
 }
