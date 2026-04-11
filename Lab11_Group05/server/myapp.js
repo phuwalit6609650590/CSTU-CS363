cb867e033a5c8f923a86fef69de78d32f6fee5ca
r <rapin.nar@dome.tu.ac.th>
2026-04-11 17:11:41 +0700

    part 2.3

--- Lab11_Group05/server/myapp.js
+++ Lab11_Group05/server/myapp.js
@@ -1,29 +1,36 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is my home page");
+  } else if (req.url === "/members_text") {
+    const filePath = path.join(__dirname, "members.html");
 
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('This is my home page');
-    } 
-    
-    else if (req.url === '/members_text') {
-        const filePath = path.join(__dirname, 'members.html');
-        
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-            res.end(data);
-        });
-    }  
+    fs.readFile(filePath, "utf8", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end(data);
+    });
+  } else if (req.url === "/members_html") {
+    // ส่วนที่ 2 ข้อ 3 อ่านไฟล์และส่งกลับเป็น HTML
+    const filePath = path.join(__dirname, "members.html");
 
-    else if (req.url === '/members_html') {
-        //ส่วนที่ 2 ข้อ 3 เอาโค้ดลงdocด้วย
-    } 
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Error loading members.html");
+        return;
+      }
+      // text/html
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(data);
+    });
+  }
 });
 
 //ส่วนที่ 2 ข้อ 4 รันแล้วเขียนเปรียบเทียบในdoc
 server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
