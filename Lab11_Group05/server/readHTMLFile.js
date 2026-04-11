24c98a88347c2fae6e425cf7d2c1fea7571bdd47
Jutinut-BBBOMB <kirikaya17@gmail.com>
2026-04-11 13:28:57 +0700

    add readHTMLFile

--- Lab11_Group05/server/readHTMLFile.js
+++ Lab11_Group05/server/readHTMLFile.js
@@ -2,3 +2,18 @@
 
 function readHTML(htmlFileName) {}
 
+const fs = require('fs');
+
+function readHTML(htmlFileName) {
+    fs.readFile(htmlFileName, 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error:", err);
+            return;
+        }
+        console.log(`---${htmlFileName} ---`);
+        console.log(data);
+    });
+}
+
+// Export ฟังก์ชันออกไปให้ไฟล์อื่นเรียกใช้ 
+module.exports = { readHTML };
