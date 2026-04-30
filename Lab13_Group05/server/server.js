e857466e41a94374c2b5c27fb5a32b46bc0911b6
Phuwalit <phuwalit.jar@dome.tu.ac.th>
2026-04-30 19:26:48 +0700

    e1.2 commit

--- Lab13_Group05/server/server.js
+++ Lab13_Group05/server/server.js
@@ -13,6 +13,19 @@ app.get('/api/products/all', async(req, res)=>{
 
     return res.status(200).send(parseData.items)
 })
+app.get('/api/product/:id', async(req, res) => {
+    
+        const targetId = req.params.id;
+        const data = await fs.readFile('./it-gadgets.json', 'utf-8');
+        const parseData = JSON.parse(data);
+
+        const foundItem = parseData.items.find(item => String(item.id) === targetId);
+        if (!foundItem) {
+            return res.status(404);
+        }
+        return res.status(200).send(foundItem);
+
+})
 
 app.listen(3001, ()=>{
     console.log('Server on port 3001')
